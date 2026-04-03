4db5f25c10522a565604341a2e1d530e1dbcb55a
Eva-Mignerey <eva.mignerey@etudiant.univ-reims.fr>
2026-04-03 10:48:49 +0200

    ajout mixins

--- src/views/Connexion.vue
+++ src/views/Connexion.vue
@@ -2,7 +2,8 @@
 
 <template>
     <body class="body-connexion">
-        <img src="../assets/images/logo_collecte.svg" alt="logo" class="logo" />
-        <h1 class="titre-logo">Bienvenue sur<br>CO<sub>2</sub>llecte !</h1>
+        <img src="../assets/images/logo_collecte.svg" alt="logo" class="logo" />        <h1 class="titre-logo">Bienvenue sur<br>CO<sub>2</sub>llecte !</h1>
+        <a href="" class="btn btn-vert">Connecte-toi !</a>
+        <h2>1</h2>
     </body>
 </template>
